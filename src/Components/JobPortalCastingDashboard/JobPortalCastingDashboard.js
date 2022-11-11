38735ec648b8e773647fadfcde7878ef2a2e1169
Amisha1008 <sahuamisha788@gmail.com>
2022-11-11 18:14:52 +0530

    casting dashboard, job portal with all modals done

--- src/Components/JobPortalCastingDashboard/JobPortalCastingDashboard.js
+++ src/Components/JobPortalCastingDashboard/JobPortalCastingDashboard.js
@@ -1,10 +1,111 @@
-import React from "react";
-import { Button, Col, Row, Table } from "react-bootstrap";
+import React, {useState} from "react";
+import { Button, Col, Row, Table, Modal } from "react-bootstrap";
 import "./JobPortalCastingDashboard.css";
+import { IoCloseSharp } from "react-icons/io5";
 
 export default function JobPortalCastingDashboard() {
+  const [showViewModal, setShowViewModal] = useState(false);
+  const [showEditModal, setShowEditModal] = useState(false);
+  const [showRemoveModal, setShowRemoveModal] = useState(false);
+
+  function ViewModal(props) {
+    return (
+      <Modal
+        {...props}
+        size="md"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header id="viewModalHeader">
+          <IoCloseSharp
+            id="closeIcon"
+            onClick={() => setShowViewModal(!showViewModal)}
+          />
+        </Modal.Header>
+        <Modal.Body id="viewModalBody">
+          <h6>TF ID : #54356</h6>
+          <h6>
+            Name : Rudra N Dash <br />
+            Experience : 3years <br />
+            Role : Production manager
+          </h6>
+        </Modal.Body>
+      </Modal>
+    );
+  }
+
+  function EditModal(props) {
+    return (
+      <Modal
+        {...props}
+        size="md"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header id="viewModalHeader">
+          <IoCloseSharp
+            id="closeIcon"
+            onClick={() => setShowEditModal(!showEditModal)}
+          />
+        </Modal.Header>
+        <Modal.Body id="viewModalBody">
+          <h6>TF ID : #54356</h6>
+          <h6>
+            Name : Rudra N Dash <br />
+            Experience : 3years <br />
+            Role : Production manager
+          </h6>
+        </Modal.Body>
+        <Modal.Footer id="viewModalFooter">
+          <Button onClick={() => setShowEditModal(!showEditModal)}>Save</Button>
+        </Modal.Footer>
+      </Modal>
+    );
+  }
+
+  function RemoveModal(props) {
+    return (
+      <Modal
+        {...props}
+        size="md"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header id="viewModalHeader">
+          <IoCloseSharp
+            id="closeIcon"
+            onClick={() => setShowRemoveModal(!showRemoveModal)}
+          />
+        </Modal.Header>
+        <Modal.Body id="viewModalBody" style={{ textAlign: "center" }}>
+          <h6>Are you sure want to delete ?</h6>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "center",
+            }}
+          >
+            <Button
+              variant="danger"
+              onClick={() => setShowRemoveModal(!showRemoveModal)}
+            >
+              Delete
+            </Button>
+            <Button
+              style={{ background: "#1931bb", marginLeft: "0.5rem" }}
+              onClick={() => setShowRemoveModal(!showRemoveModal)}
+            >
+              Cancel
+            </Button>
+          </div>
+        </Modal.Body>
+      </Modal>
+    );
+  }
+
   return (
-    <Row style={{overflowX: 'scroll'}}>
+    <Row style={{ overflowX: "scroll" }}>
       <Col>
         <Table striped>
           <thead>
@@ -28,11 +129,26 @@ export default function JobPortalCastingDashboard() {
               <td>#5456456</td>
               <td>22</td>
               <td>1 yr</td>
-              <td style={{color: 'green'}}>Selected</td>
-              <td style={{display: 'flex', justifyContent: 'flex-end'}}>
-                <Button id="buttons">View</Button>
-                <Button id="buttons">Edit</Button>
-                <Button id="removeBtn">Remove</Button>
+              <td style={{ color: "green" }}>Selected</td>
+              <td style={{ display: "flex", justifyContent: "flex-end" }}>
+                <Button
+                  id="buttons"
+                  onClick={() => setShowViewModal(!showViewModal)}
+                >
+                  View
+                </Button>
+                <Button
+                  id="buttons"
+                  onClick={() => setShowEditModal(!showEditModal)}
+                >
+                  Edit
+                </Button>
+                <Button
+                  id="removeBtn"
+                  onClick={() => setShowRemoveModal(!showRemoveModal)}
+                >
+                  Remove
+                </Button>
                 <Button id="buttons">Share</Button>
               </td>
             </tr>
@@ -42,16 +158,45 @@ export default function JobPortalCastingDashboard() {
               <td>#5456456</td>
               <td>22</td>
               <td>1 yr</td>
-              <td style={{color: 'red'}}>Rejected</td>
-              <td style={{display: 'flex', justifyContent: 'flex-end'}}>
-                <Button id="buttons">View</Button>
-                <Button id="buttons">Edit</Button>
-                <Button id="removeBtn">Remove</Button>
+              <td style={{ color: "red" }}>Rejected</td>
+              <td style={{ display: "flex", justifyContent: "flex-end" }}>
+                <Button
+                  id="buttons"
+                  onClick={() => setShowViewModal(!showViewModal)}
+                >
+                  View
+                </Button>
+                <Button
+                  id="buttons"
+                  onClick={() => setShowEditModal(!showEditModal)}
+                >
+                  Edit
+                </Button>
+                <Button
+                  id="removeBtn"
+                  onClick={() => setShowRemoveModal(!showRemoveModal)}
+                >
+                  Remove
+                </Button>
                 <Button id="buttons">Share</Button>
               </td>
             </tr>
           </tbody>
         </Table>
+
+        {/* modals */}
+        <ViewModal
+          show={showViewModal}
+          onHide={() => setShowViewModal(false)}
+        />
+        <EditModal
+          show={showEditModal}
+          onHide={() => setShowEditModal(false)}
+        />
+        <RemoveModal
+          show={showRemoveModal}
+          onHide={() => setShowRemoveModal(false)}
+        />
       </Col>
     </Row>
   );
--- src/Components/MyDataCastingDashboard/MyDataCastingDashboard.js
+++ src/Components/MyDataCastingDashboard/MyDataCastingDashboard.js
@@ -1,9 +1,110 @@
-import React from "react";
-import { Button, Col, Row, Table } from "react-bootstrap";
+import React, { useState } from "react";
+import { Button, Col, Row, Table, Modal } from "react-bootstrap";
 import { BsPlusSquareFill } from "react-icons/bs";
+import { IoCloseSharp } from "react-icons/io5";
 import "./MyDataCastingDashboard.css";
 
 export default function MyDataCastingDashboard() {
+  const [showViewModal, setShowViewModal] = useState(false);
+  const [showEditModal, setShowEditModal] = useState(false);
+  const [showRemoveModal, setShowRemoveModal] = useState(false);
+
+  function ViewModal(props) {
+    return (
+      <Modal
+        {...props}
+        size="md"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header id="viewModalHeader">
+          <IoCloseSharp
+            id="closeIcon"
+            onClick={() => setShowViewModal(!showViewModal)}
+          />
+        </Modal.Header>
+        <Modal.Body id="viewModalBody">
+          <h6>TF ID : #54356</h6>
+          <h6>
+            Name : Rudra N Dash <br />
+            Experience : 3years <br />
+            Role : Production manager
+          </h6>
+        </Modal.Body>
+      </Modal>
+    );
+  }
+
+  function EditModal(props) {
+    return (
+      <Modal
+        {...props}
+        size="md"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header id="viewModalHeader">
+          <IoCloseSharp
+            id="closeIcon"
+            onClick={() => setShowEditModal(!showEditModal)}
+          />
+        </Modal.Header>
+        <Modal.Body id="viewModalBody">
+          <h6>TF ID : #54356</h6>
+          <h6>
+            Name : Rudra N Dash <br />
+            Experience : 3years <br />
+            Role : Production manager
+          </h6>
+        </Modal.Body>
+        <Modal.Footer id="viewModalFooter">
+          <Button onClick={() => setShowEditModal(!showEditModal)}>Save</Button>
+        </Modal.Footer>
+      </Modal>
+    );
+  }
+
+  function RemoveModal(props) {
+    return (
+      <Modal
+        {...props}
+        size="md"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header id="viewModalHeader">
+          <IoCloseSharp
+            id="closeIcon"
+            onClick={() => setShowRemoveModal(!showRemoveModal)}
+          />
+        </Modal.Header>
+        <Modal.Body id="viewModalBody" style={{ textAlign: "center" }}>
+          <h6>Are you sure want to delete ?</h6>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "center",
+            }}
+          >
+            <Button
+              variant="danger"
+              onClick={() => setShowRemoveModal(!showRemoveModal)}
+            >
+              Delete
+            </Button>
+            <Button
+              style={{ background: "#1931bb", marginLeft: '0.5rem' }}
+              onClick={() => setShowRemoveModal(!showRemoveModal)}
+            >
+              Cancel
+            </Button>
+          </div>
+        </Modal.Body>
+      </Modal>
+    );
+  }
+
   return (
     <Row>
       <Col>
@@ -26,9 +127,24 @@ export default function MyDataCastingDashboard() {
                 <td>Rudra Narayan Dash</td>
                 <td>#5456456</td>
                 <td style={{ display: "flex", justifyContent: "flex-end" }}>
-                  <Button id="buttons">View</Button>
-                  <Button id="buttons">Edit</Button>
-                  <Button id="removeBtn">Remove</Button>
+                  <Button
+                    id="buttons"
+                    onClick={() => setShowViewModal(!showViewModal)}
+                  >
+                    View
+                  </Button>
+                  <Button
+                    id="buttons"
+                    onClick={() => setShowEditModal(!showEditModal)}
+                  >
+                    Edit
+                  </Button>
+                  <Button
+                    id="removeBtn"
+                    onClick={() => setShowRemoveModal(!showRemoveModal)}
+                  >
+                    Remove
+                  </Button>
                   <Button id="buttons">Share</Button>
                 </td>
               </tr>
@@ -37,14 +153,43 @@ export default function MyDataCastingDashboard() {
                 <td>Rudra Narayan Dash</td>
                 <td>#5456456</td>
                 <td style={{ display: "flex", justifyContent: "flex-end" }}>
-                  <Button id="buttons">View</Button>
-                  <Button id="buttons">Edit</Button>
-                  <Button id="removeBtn">Remove</Button>
+                  <Button
+                    id="buttons"
+                    onClick={() => setShowViewModal(!showViewModal)}
+                  >
+                    View
+                  </Button>
+                  <Button
+                    id="buttons"
+                    onClick={() => setShowEditModal(!showEditModal)}
+                  >
+                    Edit
+                  </Button>
+                  <Button
+                    id="removeBtn"
+                    onClick={() => setShowRemoveModal(!showRemoveModal)}
+                  >
+                    Remove
+                  </Button>
                   <Button id="buttons">Share</Button>
                 </td>
               </tr>
             </tbody>
           </Table>
+
+          {/* modals */}
+          <ViewModal
+            show={showViewModal}
+            onHide={() => setShowViewModal(false)}
+          />
+          <EditModal
+            show={showEditModal}
+            onHide={() => setShowEditModal(false)}
+          />
+          <RemoveModal
+            show={showRemoveModal}
+            onHide={() => setShowRemoveModal(false)}
+          />
         </Row>
       </Col>
     </Row>
